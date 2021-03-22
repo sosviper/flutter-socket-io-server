044406780b0ce58c06d941bc57001acbdccb9d1e
Victor Valencia <sosviperdigital@gmail.com>
2021-03-22 07:39:48 -0500

    Cambios sobre bandas terminado

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,8 +1,21 @@
 const { io } = require('../index');
+const Bands = require('../models/bands');
+const Band = require('../models/band');
+
+const bands = new Bands();
+
+bands.addBand( new Band( 'Queen' ) );
+bands.addBand( new Band( 'Bon Jovi' ) );
+bands.addBand( new Band( 'Héroes del Silencio' ) );
+bands.addBand( new Band( 'Metallica' ) );
+
+// console.log(bands);
 
 // Mensajes de sockets
 io.on('connection', client => {
     console.log('Cliente conectado');
+
+    client.emit('active-bands', bands.getBands() );
 
     client.on('disconnect', () => {
         console.log('Cliente desconectado');
@@ -10,8 +23,38 @@ io.on('connection', client => {
 
     client.on('mensaje', ( payload ) => {
         console.log('Mensaje', payload);
-
         io.emit( 'mensaje', { admin: 'Nuevo mensaje' } );
     });
+
+    client.on('vote-band', ( payload ) => {
+
+        bands.voteBand( payload.id );
+        io.emit('active-bands', bands.getBands() );
+
+    });
+
+    // Escuchar: add-band
+    client.on('add-band', ( payload ) => {
+
+        const newBand = new Band( payload.name );
+        bands.addBand( newBand );
+        io.emit('active-bands', bands.getBands() );
+
+    });
+
+    // delete-band
+    client.on('delete-band', ( payload ) => {
+
+        bands.deleteBand( payload.id );
+        io.emit('active-bands', bands.getBands() );
+
+    });
+
+    // client.on('emitir-mensaje', ( payload ) => {
+    //     // console.log(payload);
+    //     // io.emit('nuevo-mensaje', payload ); // emote a todos!
+    //     client.broadcast.emit('nuevo-mensaje', payload); // emite a todos menos a quien envia le mensaje
+
+    // });
 
 });
